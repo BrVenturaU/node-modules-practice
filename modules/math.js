2d2fcf1630bac41e2852131d676a6cdee1977990
BrVenturaU <brandonventura16@gmail.com>
2021-05-18 15:49:55 -0600

    Agrega las cuatro operaciones básicas.

---
+++ modules/math.js
@@ -0,0 +1,13 @@
+
+
+const add = (number1, number2) => number1 + number2;
+const subtract = (number1, number2) => number1 - number2;
+const multiply = (number1, number2) => number1 * number2;
+const divide = (number1, number2) => number1 / number2;
+
+module.exports={
+    add,
+    subtract, 
+    multiply,
+    divide
+};
