555ad6c32ea4c41732c9dd1d68889c7da4afb0b1
BrVenturaU <brandonventura16@gmail.com>
2021-05-18 20:52:16 -0600

    Agrega endpoint que retorna HTML de Posts

--- modules/express-init-server.js
+++ modules/express-init-server.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let axios = require('./post-module');
 
 express.initServer = (initialPath, port) => {
     const app = express();
@@ -6,7 +7,20 @@ express.initServer = (initialPath, port) => {
         res.send('<h1>This is your first path with express.</h1>');
         res.end();
     });
-
+    
+    app.get('/posts', (req, res) => {
+        axios.get(3).then(posts =>{
+            let html = `<h1>Listado de Posts</h1>`;
+            posts.map(post => {
+                html += `<div>
+                    <h3>${post.title}</h3>
+                    <p>${post.body}</p>
+                </div>`
+            });
+            res.send(html);
+            res.end();
+        })
+    });
     express.server = app.listen(port, () => {
         console.log(`Express is running at port: "${port}" and initial path: "${initialPath}".`);
     });
